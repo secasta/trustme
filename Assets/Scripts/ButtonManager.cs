ff4a931e43907a534e1fdca8606a8e6e3d0364ef
Daniel Tomas <ticidici@yahoo.es>
2017-02-25 17:59:01 +0100

    Add Button Manager. Simple transition to rat scene and back.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadStorySelectScreen()
+    {
+        SceneManager.LoadScene("_StorySelect");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
