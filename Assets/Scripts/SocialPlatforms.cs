ccc98480bc141b1a57acd2932aa5b6c8b55de9cd
Daniel Tomas <ticidici@yahoo.es>
2017-04-20 13:48:51 +0200

    Start working on Game Center implementation. Should check if user is logged in and show the state of achievements. But no unlock yet. Haven't tested it yet, though.

---
+++ Assets/Scripts/SocialPlatforms.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.SocialPlatforms.GameCenter;
+
+public class SocialPlatforms : MonoBehaviour {
+
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+
+        GameCenterPlatform.ShowDefaultAchievementCompletionBanner(true);
+
+        Social.localUser.Authenticate(success => {
+            if (success)
+            {
+                ReportAchievement();
+            }
+            else
+            {
+                Debug.Log("Failed to authenticate");
+            }
+        });
+    }
+
+    void ReportAchievement()
+    {
+        Debug.Log("Authenticated, checking achievements");
+        Social.LoadAchievements(ProcessLoadedAchievements);
+    }
+
+    void ProcessLoadedAchievements(IAchievement[] achievements)
+    {
+        if (achievements.Length == 0)
+        {
+            Debug.Log("Error: no achievements found");
+        }
+        else
+        {
+            Debug.Log("Got " + achievements.Length + " achievements");
+        }
+
+    }
+}
