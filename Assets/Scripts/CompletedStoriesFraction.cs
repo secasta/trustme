2de527fcbb3fea3e5aedb07b6bb086983bad778a
Daniel Tomas <ticidici@yahoo.es>
2017-05-04 02:13:59 +0200

    Fraction of completed stories vs total stories implemented. The total number is a public variable, so we can fake it if we need to, hehe.

---
+++ Assets/Scripts/CompletedStoriesFraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompletedStoriesFraction : MonoBehaviour {
+
+    public int _totalStories;
+
+    private Text _fractionText;
+    private int _numberOfUnlockedStories;
+
+    void Awake()
+    {
+        _fractionText = GetComponent<Text>();
+        if (!_fractionText) { Debug.LogError("No text component found!", this); }
+    }
+
+	void Start ()
+    {
+        _numberOfUnlockedStories = Game.current._unlockedStories.Count;
+        _fractionText.text = _numberOfUnlockedStories + "/" + _totalStories;
+	}
+
+    void OnEnable()
+    {
+        CanvasManager.OnStoryCompleted += IncrementNumberOfUnlockedStories;
+    }
+
+    void OnDisable()
+    {
+        CanvasManager.OnStoryCompleted -= IncrementNumberOfUnlockedStories;
+    }
+
+    void IncrementNumberOfUnlockedStories(int id)//In this case we don't care about the id, but we need to get it to handle this event
+    {
+        _numberOfUnlockedStories++;
+        StartCoroutine(ChangeText());
+    }
+
+    IEnumerator ChangeText()
+    {
+        yield return new WaitForSeconds(2f);//meh, a ojo
+        _fractionText.text = _numberOfUnlockedStories + "/" + _totalStories;
+    }
+}
