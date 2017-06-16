430b62d4c277a1c01426777c0004c25c438b9888
Daniel Tomas <ticidici@yahoo.es>
2017-06-16 19:56:45 +0200

    Moving clouds in the background. New animation for Play button. Now it appears in fashion.

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+    public float _speed = -0.2f;
+    [Range(-2000, -1250)]
+    public int _leftTrigger = -1600;
+    [Range(1250, 2000)]
+    public int _rightTrigger = 1600;
+
+    void Update ()
+    {
+        Vector3 aux = transform.localPosition;
+        if (transform.localPosition.x < _leftTrigger)
+        {
+            aux.x = _rightTrigger;
+            transform.localPosition = aux;
+        }
+        else
+        {
+            aux = transform.localPosition;
+            aux.x += _speed * Time.deltaTime;
+            transform.localPosition = aux;
+        }
+    }
+}
